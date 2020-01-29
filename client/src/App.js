90a3c9f50f0887efad67c24c754abd85a0eb1fd5
Michael Ross <mross.j@gmail.com>
2020-01-28 20:21:36 -0500

    wrap App, add routes for MovieList and id ; MovieList link, Part 2 cont...

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
-
-import SavedList from './Movies/SavedList';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import MovieList from "./Movies/MovieList";
+import SavedList from "./Movies/SavedList";
+import Movie from "./Movies/Movie";
 
 const App = () => {
-  const [savedList, setSavedList] = useState( [] );
+	const [savedList, setSavedList] = useState([]);
 
-  const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+	const addToSavedList = movie => {
+		setSavedList([...savedList, movie]);
+	};
 
-  return (
-    <div>
-      <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
-    </div>
-  );
+	return (
+		<div>
+			<SavedList list={savedList} />
+			<Route exact path="/" component={MovieList} />
+			<Route
+				exact
+				path="/movies/:id"
+				component={Movie}
+				addToSavedList={addToSavedList}
+			/>
+			<div>Replace this Div with your Routes</div>
+		</div>
+	);
 };
 
 export default App;
