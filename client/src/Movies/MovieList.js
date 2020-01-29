2ad391870db74157158e5e0daf20ecff970e752f
Michael Ross <mross.j@gmail.com>
2020-01-29 11:00:08 -0500

    refactor MovieList.js, create new component in MovieCard, return MovieCard ;

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import MovieCard from './MovieCard';
 
 const MovieList = props => {
 	const [movies, setMovies] = useState([]);
@@ -29,25 +30,10 @@ const MovieList = props => {
 };
 
 function MovieDetails({ movie }) {
-	const { title, director, metascore, stars, id } = movie;
+	const { id } = movie;
 	return (
 		<Link to={`/movies/${id}`}>
-			<div className='movie-card'>
-				<h2>{title}</h2>
-				<div className='movie-director'>
-					Director: <em>{director}</em>
-				</div>
-				<div className='movie-metascore'>
-					Metascore: <strong>{metascore}</strong>
-				</div>
-				<h3>Actors</h3>
-
-				{stars.map(star => (
-					<div key={star} className='movie-star'>
-						{star}
-					</div>
-				))}
-			</div>
+			<MovieCard movie={movie} />
 		</Link>
 	);
 }
